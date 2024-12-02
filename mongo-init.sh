4b85c8bddbb1a55c562145638eb0a93e0fb3347c
Bettina Mauz <bettina.mauz@becon.de>
2024-12-02 18:39:04 +0100

    [Modified] updated to version 4.2

--- mongo-init.sh
+++ mongo-init.sh
@@ -1,1 +1,1 @@
-mongosh --eval  "db.getSiblingDB('opencelium').createUser({user:'oc_admin', pwd:'secretsecret', roles:[{role:'readWrite',db:'opencelium'},{role:'dbAdmin',db:'opencelium'}]});"
+mongosh --eval  "db.getSiblingDB('opencelium').createUser({user:'${OC_MONGODB_USER}', pwd:'${OC_MONGODB_PASSWORD}', roles:[{role:'readWrite',db:'opencelium'},{role:'dbAdmin',db:'opencelium'}]});"
